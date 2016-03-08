49aebbc0679c8fd0a68164f1f37014a6df24c1d2
Mike Schatz <mschatz@jhu.edu>
2016-03-08 16:41:29 -0500

    Eval all histograms in parallel

--- simulation/eval_simulations.sh
+++ simulation/eval_simulations.sh
@@ -3,17 +3,25 @@
 K=21
 READLEN=100
 INDIR=simulation_results
-OUTDIR=simulation_analysis
 MODELBIN=../histfitdup_bothplots.R
 
-mkdir -p $OUTDIR
 
-set -xv
+if [ $(which parallel) ] 
+then
+  echo "Running results in parallel"
+  /bin/ls $INDIR/*.fa21.hist | parallel Rscript $MODELBIN {} $K $READLEN {}
 
-for hist in Drosophila_melanogaster.BDGP6.dna_sm.main_chr.fa_het0.01_br1_rl100_cov100_err0.01_reads.fa21.hist
-do
-  echo "Processing $i"
-  Rscript $MODELBIN $INDIR/$hist $K $READLEN $OUTDIR/$hist
-done
+else
+  for hist in `/bin/ls $INDIR/*.fa21.hist`
+  do
+    echo "Processing $hist"
+    Rscript $MODELBIN $hist $K $READLEN $hist
+  done
+fi
+
+
+
+
+
 
 
