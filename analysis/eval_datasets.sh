0c551772a834a48131a5725199a70e1f312b1317
Michael Schatz <mschatz@cshl.edu>
2016-03-09 00:17:00 -0500

    Process all datasets

--- analysis/eval_datasets.sh
+++ analysis/eval_datasets.sh
@@ -2,22 +2,29 @@
 
 K=21
 READLEN=100
-INDIR=simulation_results
 MODELBIN=../histfitdup_bothplots.R
 
 
-if [ $(which parallel) ] 
-then
-  echo "Running results in parallel"
-  /bin/ls $INDIR/*21.hist | parallel -t Rscript $MODELBIN {} $K $READLEN {}
+#for INDIR in simulation_results
 
-else
-  for hist in `/bin/ls $INDIR/*21.hist`
-  do
-    echo "Processing $hist"
-    Rscript $MODELBIN $hist $K $READLEN $hist
-  done
-fi
+for INDIR in simulation_results real_data ecoli_mix
+do
+  echo "Processing $INDIR"
+
+  if [ $(which parallel) ] 
+  then
+    echo "Running results in parallel"
+    /bin/ls $INDIR/*.hist | parallel -t Rscript $MODELBIN {} $K $READLEN {}
+    #/bin/ls $INDIR/D*.hist | parallel -t Rscript $MODELBIN {} $K $READLEN {}
+
+  else
+    for hist in `/bin/ls $INDIR/*21.hist`
+    do
+      echo "Processing $hist"
+      Rscript $MODELBIN $hist $K $READLEN $hist
+    done
+  fi
+done
 
 
 
