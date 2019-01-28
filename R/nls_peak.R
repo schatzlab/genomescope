f874ad5439ecdca9d8ca47613ad6a2c27cb40157
calcnerd76 <timothy_benavidez@mail.harvard.edu>
2019-01-28 15:03:01 -0500

    fixed constraints for p=4

--- R/nls_peak.R
+++ R/nls_peak.R
@@ -151,7 +151,7 @@ nls_peak<-function(x, y, k, p, estKmercov, estLength, max_iterations) {
   if (p==4) {
     try(model <- 
     nlsLM(y ~ length*predict4(r1, r2, r3, k, d, kmercov, bias, x),
-    start = list(d=0.001, r1=0.003, r2 = 0.002, r3=0.001, kmercov=estKmercov, bias = 0.5, length = estLength/4),
+    start = list(d=0.001, r1=0.001, r2 = 0.003, r3=0.002, kmercov=estKmercov, bias = 0.5, length = estLength/4),
     lower = c(0, 0, 0, 0, 0, 0, 0),
     upper = c(1, 1, 1, 1, Inf, Inf, Inf),
     control = list(minFactor=1e-12, maxiter=max_iterations)), silent = TRUE)
@@ -162,7 +162,7 @@ nls_peak<-function(x, y, k, p, estKmercov, estLength, max_iterations) {
 
       try(model <- 
       nlsLM(y ~ length*predict4(r1, r2, r3, k, d, kmercov, bias, x),
-      start = list(d=0.001, r1=0.003, r2=0.002, r3=0.001, kmercov=estKmercov, bias = 0.5, length=estLength/4),
+      start = list(d=0.001, r1=0.001, r2=0.003, r3=0.002, kmercov=estKmercov, bias = 0.5, length=estLength/4),
       lower = c(0, 0, 0, 0, 0, 0, 0),
       upper = c(1, 1, 1, 1, Inf, Inf, Inf),
       algorithm="port", control = list(minFactor=1e-12, maxiter=max_iterations)), silent = TRUE)
