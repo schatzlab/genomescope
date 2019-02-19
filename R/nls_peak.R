107636d1d2231332561a3ce5dfce55b18a9fb36c
calcnerd76 <timothy_benavidez@mail.harvard.edu>
2019-02-19 15:45:59 -0500

    try fitting only first 100 values

--- R/nls_peak.R
+++ R/nls_peak.R
@@ -54,7 +54,8 @@ nls_peak<-function(x, y, k, p, top, estKmercov, estLength, max_iterations) {
     r_text = paste(paste(lapply(1:(num_r), function(x) paste("r", as.character(x), sep="")), collapse=", "), ", ")
   }
   if (TRANSFORM) {
-    y_transform = x*y
+    x = head(x,100)
+    y_transform = head(x,100)*head(y,100)
     formula = as.formula(paste("y_transform ~ x*length*predict",p,"_",top,"(",r_text, "k, d, kmercov, bias, x)",sep=""))
   } else {
     formula = as.formula(paste("y ~ length*predict",p,"_",top,"(",r_text, "k, d, kmercov, bias, x)",sep=""))
