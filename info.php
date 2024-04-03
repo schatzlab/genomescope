b72debd8505f208813187c7e6664a3bad55f7b9f
Michael Schatz <michael.schatz@gmail.com>
2024-04-03 17:23:00 -0400

    update info page

--- info.php
+++ info.php
@@ -14,17 +14,18 @@
       <div class="panel-body">
         <p>The code is open source on Github: <a href="https://github.com/schatzlab/genomescope" target="_blank">github.com/schatzlab/genomescope</a></p>
         <br>
-        <p>Statistical work by Gregory Vurture and <a href="http://schatzlab.cshl.edu/people/fsedlaze/" target="_blank">Fritz Sedlazeck</a><p>
+        <p>Statistical work by Gregory Vurture and Fritz Sedlazeck<p>
         <p>Web application by <a href="http://marianattestad.com" target="_blank">Maria Nattestad</a></p>
-        <p>Principal investigator / The Boss: <a href="http://schatzlab.cshl.edu" target="_blank">Mike Schatz</a></p>
+        <p>Principal investigator: <a href="http://schatz-lab.org" target="_blank">Mike Schatz</a></p>
         <br>
-        <p>For questions about the science, email: <a href="mailto:mschatz@cshl.edu?Subject=Question%20About%20GenomeScope" target="_top">Mike Schatz at mschatz@cshl.edu</a></p>
-        <p>For questions about the web application, email: <a href="mailto:mnattest@cshl.edu?Subject=Question%20About%20GenomeScope" target="_top">Maria Nattestad at mnattest@cshl.edu</a></p>
+        <p>For questions about the science, email: <a href="mailto:mschatz@cs.jhu.edu?Subject=Question%20About%20GenomeScope" target="_top">Mike Schatz at mschatz@cs.jhu.edu</a></p>
+        <br>
+        <p>Please cite: <a href="https://doi.org/10.1093/bioinformatics/btx153">Vurture et al. (2017) GenomeScope: Fast reference-free profiling from short reads. Bioinformatics. doi: https://doi.org/10.1093/bioinformatics/btx153</a></p>
+        <br>
+        <p>Also try out <a href="http://genomescope.org/genomescope2">GenomeScope 2.0</a></p>
       </div>
     </div>
 </div>
-
-
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
